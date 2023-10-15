ae2f6c7beb315cf94538603b1e5076db2df688d4
mohamed gasmi <75278320+mohamedgasmicrypten2020@users.noreply.github.com>
2023-10-15 02:44:23 +0100

    addfast

---
+++ JsonServer_fetch-data-master/index.js
@@ -0,0 +1,48 @@
+
+let tbody = document.getElementById("tbody")
+
+
+
+// fetch function
+fetch("http://localhost:3000/user")
+    .then(res => res.json())
+    .then(json => {
+        json.map(data => {
+            console.log(data)
+            tbody.append(td_fun(data));
+        })
+    })
+
+// create td
+function td_fun({ profile, game, email, playtime, genre,platform}){
+    let td = document.createElement('tr');
+    td.innerHTML = `
+    <td class="px-6 py-4 whitespace-nowrap">
+        <div class="flex items-center">
+                <div class="flex-shrink-0 h-10 w-10">
+                    <img src="${profile}" class="h-10 w-10 rounded-full" alt="">
+                </div>
+                <div class="ml-4">
+                    <div class="text-sm font-medium text-gray-900">
+                        ${game}
+                    </div>
+                    <div class="text-sm text-gray-500">
+                        ${email}
+                    </div>
+                </div>
+            </div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+        <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+           ${playtime}
+        </span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+        <span class="text-sm text-gray-500">${genre}</span>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap">
+        <span class="text-sm text-gray-500">${platform}</span>
+    </td>
+    `;
+    return td;
+}
